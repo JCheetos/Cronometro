d48033c537e57eb18f6449860bdabc366b34bdfb
JCheetos <yosoycheetos@outlook.com>
2018-02-22 07:29:09 -0500

    Prueba 1

--- Cronometro/src/cronometro/Principal.java
+++ Cronometro/src/cronometro/Principal.java
@@ -5,6 +5,8 @@
  */
 package cronometro;
 
+import cronometro.logica.Cronometro;
+
 
 
 /**
@@ -15,7 +17,19 @@ public class Principal {
 
     
     public static void main(String[] args) {
+        Cronometro c = new Cronometro();
+        int o=0;
+        for (int i = 0 ; i <864000; i++){
+            c.avanzar();
+            System.out.println(c.obtenerTiempo());
+            if(i%100 == 0){
+                c.guardarMemoria(o);
+                o++;
+            }
+        }
+        System.out.println();
+        System.out.println("Memorias: ");
+        c.mostrarMemorias();
         
     }
-    
 }
--- Cronometro/src/cronometro/logica/Memoria.java
+++ Cronometro/src/cronometro/logica/Memoria.java
@@ -5,63 +5,28 @@
  */
 package cronometro.logica;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
 /**
  *
  * @author Estudiantes
  */
-public class Cronometro {
+public class Memoria {
     private UnidadTiempo decimas;
     private UnidadTiempo segundos;
     private UnidadTiempo minutos;
     private UnidadTiempo horas;
-    private ArrayList<Memoria> memorias;
     
-    public Cronometro () {
+    public Memoria() {
         decimas = new UnidadTiempo(10);
         segundos = new UnidadTiempo(60);
         minutos = new UnidadTiempo(60);
         horas = new UnidadTiempo(24);
-        memorias = new ArrayList<Memoria>[10];
     }
-    
-    public void avanzar() {
-        
-        decimas.avanzar();
-        if (decimas.getValor() == 0) {
-            segundos.avanzar();
-            if (segundos.getValor() == 0) {
-                minutos.avanzar();
-                if (minutos.getValor()==0){
-                    horas.avanzar();
-                }
-            }
-        }
-    }
-    
+
     public String obtenerTiempo() {
         return horas.obtenerValorFormateado()+" : "+
                minutos.obtenerValorFormateado()+" : "+
                segundos.obtenerValorFormateado()+" : "+
                decimas.obtenerValorFormateado();
-    }
-
-    public void guardarMemoria(int o) {
-        Memoria m = new Memoria();
-        m.setDecimas(decimas);
-        m.setSegundos(segundos);
-        m.setMinutos(minutos);
-        m.setHoras(horas);
-        memorias.set(o, m);
-    }
-    
-    public void mostrarMemorias(){
-        for (Iterator<Memoria> iterator = memorias.iterator(); iterator.hasNext();){
-            Memoria next = iterator.next();
-            System.out.println(next.obtenerTiempo());
-        }
     }
     
     public UnidadTiempo getDecimas() {
@@ -95,4 +60,5 @@ public class Cronometro {
     public void setHoras(UnidadTiempo horas) {
         this.horas = horas;
     }
+    
 }
