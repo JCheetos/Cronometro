229d15cac55c2618707dd3034085eee84dcbea09
JCheetos <>
2018-02-26 07:32:31 -0500

    Actualizacion

--- Cronometro/src/cronometro/Principal.java
+++ Cronometro/src/cronometro/Principal.java
@@ -6,22 +6,42 @@
 package cronometro;
 
 import cronometro.logica.Cronometro;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  *
  * @author Estudiantes
  */
 public class Principal {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Cronometro c = new Cronometro();
         
+        TimerTask timerTask ;
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                c.avanzar();
+                System.out.println(c.obtenerTiempo());
+            }
+        };
+        TimerTask timerTask2 ;
+        timerTask2 = new TimerTask() {
+            @Override
+            public void run() {
+                c.retroceder();
+                System.out.println(c.obtenerTiempo());
+            }
+        };
+        Timer timer = new Timer();      
+        
+        
         for (int i = 0; i < 1000; i++) {
-            c.avanzar();
-            System.out.println(c.obtenerTiempo());
+            timer.scheduleAtFixedRate(timerTask, 0, 100);
             if(i % 100 == 0){
                 c.guardarMemoria();
             }
@@ -33,7 +53,7 @@ public class Principal {
         System.out.println("Retrocediendo:");
         
         for (int i = 0; i < 1000; i++) {
-            c.retroceder();
+            timer.scheduleAtFixedRate(timerTask2, 0, 100);
         }
         
     }
