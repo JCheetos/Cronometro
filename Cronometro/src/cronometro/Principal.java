816fa5754fe2d05f9474f3edb8be4ecc554d267e
JCheetos <yosoycheetos@outlook.com>
2018-03-01 07:06:53 -0500

    Actualizacion GUI

--- Cronometro/src/cronometro/Principal.java
+++ Cronometro/src/cronometro/Principal.java
@@ -14,62 +14,58 @@ import java.util.TimerTask;
  * @author Estudiantes
  */
 public class Principal {
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Cronometro c = new Cronometro();
         final int a = 0;
-        
-        Timer timer = new Timer();  
-        
+
+        Timer timer = new Timer();
+
         TimerTask task;
-        task = new TimerTask(){
-            int contador=0;
+        task = new TimerTask() {
+            int contador = 0;
+
             @Override
             public void run() {
                 c.avanzar();
                 System.out.println(c.obtenerTiempo());
                 contador++;
-                if(contador % 100 == 0){
+                if (contador % 100 == 0) {
                     c.guardarMemoria();
                 }
             }
         };
-        
-        TimerTask task2 ;
+
+        TimerTask task2;
         task2 = new TimerTask() {
-            int contador=0;
+            int contador = 0;
+
             @Override
             public void run() {
                 c.retroceder();
                 System.out.println(c.obtenerTiempo());
             }
         };
-        
+
         timer.scheduleAtFixedRate(task, 0, 100);
-        try
-        {
-        Thread.sleep(15000);
-        }
-        catch (Exception e)
-        {
+        try {
+            Thread.sleep(15000);
+        } catch (Exception e) {
         }
         task.cancel();
-        
+
         System.out.println("Memorias:");
         c.mostrarMemorias();
-        
+
         System.out.println("Retrocediendo:");
         timer.scheduleAtFixedRate(task2, 0, 100);
-        
-        try
-        {
-        Thread.sleep(15001);
-        }
-        catch (Exception e)
-        {
+
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
         }
         task2.cancel();
         timer.cancel();
